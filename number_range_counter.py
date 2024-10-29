83e425aacca36e0b2fe7899e3ede56fc473bafe8
clarencejeddmaas <clarencemaas74@gmail.com>
2024-10-29 19:08:40 +0800

    Define function and start a dictionary

---
+++ number_range_counter.py
@@ -0,0 +1,10 @@
+# 1. Define the function
+def count_number_range(number_range): 
+    # 2. Initialize a dictionary to store counts for each range
+    counts_in_range = {
+        "1-10": 0,
+        "11-20": 0,
+        "21-30": 0,
+        "31-40": 0,
+        "41-50": 0,
+    }
