cfa615b9cc96ddadf3c1e8b5bc3f2e8ff164d85b
clarencejeddmaas <clarencemaas74@gmail.com>
2024-10-29 19:24:22 +0800

    Go through each number in the list and update the count for its range.

--- number_range_counter.py
+++ number_range_counter.py
@@ -8,3 +8,19 @@ def count_number_range(number_range):
         "31-40": 0,
         "41-50": 0,
     }
+
+     # 3. Go through each number in the input list
+    for number in number_range:
+        # 4. Check which range the number belongs to and update the count
+        if 1 <= number <= 10:
+            counts_in_range["1-10"] += 1
+        elif 11 <= number <= 20:
+            counts_in_range["11-20"] += 1
+        elif 21 <= number <= 30:
+            counts_in_range["21-30"] += 1
+        elif 31 <= number <= 40:
+            counts_in_range["31-40"] += 1
+        elif 41 <= number <= 50:
+            counts_in_range["41-50"] += 1
+        else:
+            print("Error! Number exceeded in given range.")
